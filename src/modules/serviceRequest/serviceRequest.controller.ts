c9e85c30f09077116806e6e53ae2348a5f07d10f
Bemin Raafat <beminraafat@gmail.com>
2026-06-21 01:42:40 +0300

    feat: add serviceRequest controller

---
+++ src/modules/serviceRequest/serviceRequest.controller.ts
@@ -0,0 +1,63 @@
+import { asyncHandler } from '../../utils/asyncHandler.js';
+import {
+  ServiceRequestParams,
+  ServiceRequestQuery,
+} from './serviceRequest.schema.js';
+import * as serviceRequestService from './serviceRequest.service.js';
+
+const getUploadedFiles = (files: Express.Multer.File[] | undefined) => files;
+
+export const createServiceRequest = asyncHandler(async (req, res, _next) => {
+  const serviceRequest = await serviceRequestService.createServiceRequest(
+    req.user!.id,
+    req.body,
+    getUploadedFiles(req.files as Express.Multer.File[] | undefined),
+  );
+
+  res.status(201).json({ status: 'success', data: { serviceRequest } });
+});
+
+export const getServiceRequest = asyncHandler(async (req, res, _next) => {
+  const serviceRequest = await serviceRequestService.getServiceRequest(
+    (req.params as ServiceRequestParams).id,
+  );
+
+  res.status(200).json({ status: 'success', data: { serviceRequest } });
+});
+
+export const getAllServiceRequests = asyncHandler(async (req, res, _next) => {
+  if (req.params.userId) {
+    req.query.user = req.params.userId;
+  }
+
+  const { docs: serviceRequests, pagination } =
+    await serviceRequestService.getAllServiceRequests(
+      req.query as unknown as ServiceRequestQuery,
+    );
+
+  res.status(200).json({
+    status: 'success',
+    pagination,
+    data: { serviceRequests },
+  });
+});
+
+export const updateServiceRequest = asyncHandler(async (req, res, _next) => {
+  const serviceRequest = await serviceRequestService.updateServiceRequest(
+    (req.params as ServiceRequestParams).id,
+    req.user!,
+    req.body,
+    getUploadedFiles(req.files as Express.Multer.File[] | undefined),
+  );
+
+  res.status(200).json({ status: 'success', data: { serviceRequest } });
+});
+
+export const deleteServiceRequest = asyncHandler(async (req, res, _next) => {
+  await serviceRequestService.deleteServiceRequest(
+    (req.params as ServiceRequestParams).id,
+    req.user!,
+  );
+
+  res.sendStatus(204);
+});
