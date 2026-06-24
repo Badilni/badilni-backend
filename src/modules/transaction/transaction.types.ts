40aebdc8751c69d2b37d0ef0240ad0d8b23f7a13
Bemin Raafat <beminraafat@gmail.com>
2026-06-24 10:38:30 +0300

    feat: add transaction types and model

---
+++ src/modules/transaction/transaction.types.ts
@@ -0,0 +1,7 @@
+export enum TransactionType {
+  SESSION_PAYMENT = 'session_payment',
+  ESCROW_LOCK = 'escrow_lock',
+  REFUND = 'refund',
+  WELCOME_BONUS = 'welcome_bonus',
+  ADMIN_ADJUSTMENT = 'admin_adjustment',
+}
